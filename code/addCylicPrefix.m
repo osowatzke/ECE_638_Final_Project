544aec40fe7f00817c07384071469638349214f8
Owen <osowatzke@gmail.com>
2024-11-27 12:50:44 -0700

    Adding comments to cyclic prefix function

--- code/addCylicPrefix.m
+++ code/addCylicPrefix.m
@@ -1,8 +1,20 @@
+% Function adds cyclic prefix to OFDM symbols
+% Syntax:
+%
+%   Y = addCylicPrefix(x) appends default cyclic prefix length
+%   to OFDM symbols.
+%
+%   Y = addCylicPrefix(X, Name, Value) allows the user to specify
+%   additional key-value arguments given below:
+%
+%   'cyclicPrefixLen'   Cyclic prefix length
+%   'windowLen'         Window length (appended to both ends of symbol)
+% 
 function ofdmSymbolsWithPrefix = addCylicPrefix(ofdmSymbols, varargin)
     
     args = struct(...
-        'cyclicPrefixLen', 16,...
-        'windowLen', 4);
+        'cyclicPrefixLen', OFDM_DEFAULT.CYCLIC_PREFIX_LEN,...
+        'windowLen',       OFDM_DEFAULT.WINDOW_LEN);
 
     for i = 1:2:length(varargin)
         args.(varargin{i}) = varargin{i+1};
