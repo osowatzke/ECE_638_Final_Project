10cc688d8ba9456b86a709a66f6be14b330bc73b
Owen <osowatzke@gmail.com>
2024-11-27 15:36:17 -0700

    Adding comments to OFDM receiver object

--- code/ofdmReceiver.m
+++ code/ofdmReceiver.m
@@ -1,4 +1,16 @@
+% Class defines an OFDM reciever. It can be initialized as 
+% ofdmReceiver('Name','Value') where 'Name' and 'Value' are
+% property names and property values.
+% 
+% Ex: ofdmRx = ofdmReceiver('interpMethod','linear')
+%
+% Once class has been instantiated, it can be run as follows:
+% 
+% bits = ofdmRx.run(rxSignal)
+%
 classdef ofdmReceiver < keyValueInitializer
+    
+    % Public properties
     properties
         modType         = OFDM_DEFAULT.MOD_TYPE;
         modOrder        = OFDM_DEFAULT.MOD_ORDER;
@@ -15,23 +27,31 @@ classdef ofdmReceiver < keyValueInitializer
         pilotIndices    = [];
         dataIndices     = [];
     end
-    properties
+
+    % Read only properties
+    properties(SetAccess=protected)
         eqSymbols       = [];
     end
+
+    % Public class methods
     methods
-        function self = ofdmReceiver(varargin)
-            self@keyValueInitializer(varargin{:});
-        end
+
+        % Function determines received bit stream from recevied signal
         function bits = run(self, rxSignal)
 
+            % Determine received symbols with just fading (no noise)
+            % Used in perfect (unattainable) channel estimate
             fadedSymbols = self.extractSymbols(self.fadedSignal);
             fadedSymbols = fadedSymbols(self.dataIndices, :);
             
+            % Demodulate received signal
             rxSymbols = self.extractSymbols(rxSignal);
 
+            % Extract pilots and data symbols
             dataSymbols = rxSymbols(self.dataIndices,:);
-            rxPilots    = rxSymbols(self.pilotIndices,:);
+            rxPilots = rxSymbols(self.pilotIndices,:);
 
+            % Create an OFDM equalizer object
             equalizer   = ofdmEqualizer(...
                 'interpMethod',     self.interpMethod,...
                 'smoothingFilter',  self.smoothingFilter,...
@@ -44,20 +64,28 @@ classdef ofdmReceiver < keyValueInitializer
                 'pilotIndices',     self.pilotIndices,...
                 'dataIndices',      self.dataIndices);
 
+            % Equalize received symbols
             self.eqSymbols = equalizer.run(dataSymbols, rxPilots);
 
+            % Demodulate to form an array of bits
             bits = demodulateSymbols(self.eqSymbols,...
                 self.modType, self.modOrder);
 
             bits = bits(:);
         end
+
+        % Function extracts received symbols from receive signal
         function symbols = extractSymbols(self, signal)
 
+            % Determine where to place FFT window
+            % Should start after half cylic prefix and window
             symbolStart = self.cyclicPrefixLen/2 + self.windowLen + 1;
-            symbolEnd   = size(signal,1) - symbolStart + 1;
+            symbolEnd = size(signal,1) - symbolStart + 1;
 
-            symbols     = signal(symbolStart:symbolEnd,:);
-            symbols     = fft(symbols);
+            % Take FFT over FFT window. FFT will demodulate
+            % received signal to get received symbols
+            symbols = signal(symbolStart:symbolEnd,:);
+            symbols = fft(symbols);
 
         end
     end
