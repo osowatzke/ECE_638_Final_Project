4256aeb02ed0c33fead923f6f43fa16dd46907fa
Owen <osowatzke@gmail.com>
2024-11-27 12:37:29 -0700

    Simplifying modulation/demodulation function calls

--- code/demodulateSymbols.m
+++ code/demodulateSymbols.m
@@ -1,4 +1,21 @@
+% Function demodulates a sequence of symbols with desired modulation
+% type and modulation order
+%
+% Inputs:
+%   symbols     Array of symbols
+%   modType     Name of modulation type. Must be either 'qam' or 'psk'
+%   modOrder    Modulation order (# of points in constellation)
+%
+% Outputs:
+%   bits        Array of bits
+%
 function bits = demodulateSymbols(symbols, modType, modOrder)
-    demodulate = lookupDemodulationFunction(modType, modOrder);
-    bits = demodulate(symbols);
+    if strcmpi(modType, 'psk')
+        bits = pskdemod(symbols, modOrder, 'OutputType', 'bit');
+    elseif strcmpi(modType, 'qam')
+        bits = qamdemod(symbols, modOrder, 'OutputType', 'bit',...
+            'UnitAveragePower', true);
+    else
+        error('Unsupported modulation type. Select from {''QAM'',''PSK''}');
+    end
 end
--- code/modulateBits.m
+++ code/modulateBits.m
@@ -1,4 +1,21 @@
+% Function modulates a sequence of bits with desired modulation
+% type and modulation order
+%
+% Inputs:
+%   bits        Array of bits
+%   modType     Name of modulation type. Must be either 'qam' or 'psk'
+%   modOrder    Modulation order (# of points in constellation)
+%
+% Outputs:
+%   symbols     Array of symbols
+%
 function symbols = modulateBits(bits, modType, modOrder)
-    modulate = lookupModulationFunction(modType, modOrder);
-    symbols = modulate(bits);
+    if strcmpi(modType, 'psk')
+        symbols = pskmod(bits, modOrder, 'InputType', 'bit');
+    elseif strcmpi(modType, 'qam')
+        symbols = qammod(bits, modOrder, 'InputType', 'bit',...
+            'UnitAveragePower', true);
+    else
+        error('Unsupported modulation type. Select from {''QAM'',''PSK''}');
+    end
 end
