ec9ad5623b508b3230c58fc24db2a34ed3debedc
Owen <osowatzke@gmail.com>
2024-11-30 14:00:05 -0700

    Removed unintentionally added files

--- code/peakSidelobeLevelMeasurement.m
+++
@@ -1,8 +0,0 @@
-c = physconst('lightspeed');
-B = 300e6;
-rgSize = c/(2*B);
-numRangeGates = 256;
-fftSize = numRangeGates*16;
-
-x = exp(1i*2*pi*50*(0:numRangeGates-1)/numRangeGates);
-Fx = fft(x, fftSize);
