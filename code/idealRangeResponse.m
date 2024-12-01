74ab0c8ee15b80df894607d59f3b296fc857d500
Owen <osowatzke@gmail.com>
2024-12-01 12:36:09 -0700

    Adding pictures of ideal range response

--- code/idealRangeResponse.m
+++ code/idealRangeResponse.m
@@ -56,13 +56,76 @@ xlabel('Sample')
 ylabel('Magnitude (dB)')
 title('Ideal Range Response');
 
-% % Apply a window
-% Y = ifftshift(Y);
-% % win = chebwin(26,80);
-% % win = [zeros(6,1); win; 0; win; zeros(5,1)];
-% win = chebwin(53,80);
-% win = [zeros(6,1); win; zeros(5,1)];
-% win = ifftshift(win).';
-% Y = Y.*win;
-% Y = ifft(Y, );
-% plot(db(Y))
+%% Get frequency response with window
+% Get window for all non-zero subcarriers
+win = chebwin(53,80);
+padSize = N - length(win);
+win = [zeros(ceil(padSize/2),1); win; zeros(floor(padSize/2),1)].';
+
+% Apply window
+Y = Y.*win;
+
+% Plot Ideal Matched Filter Response
+w = 2*pi*k/N;
+figure(4)
+clf;
+plot(w, Y, 'LineWidth',1.5);
+grid on;
+title('Ideal Matched Filter Response')
+xlabel('Normalized Frequency (\times pi rad/sample)')
+ylabel('Magnitude');
+
+%% Get range response w/ zero-padding
+% Determine the Range Response
+fftSize = 16*N;
+Ypad = [zeros(1, fftSize/2-N/2), Y, zeros(1, fftSize/2-N/2)];
+Yshift = ifftshift(Ypad);
+y = ifft(Yshift, fftSize);
+y = y/max(abs(y));
+n = (0:(length(y)-1));
+figure(5)
+clf;
+plot(n,db(y),'LineWidth',1.5);
+grid on;
+xlim([n(1) n(end)])
+xlabel('Sample')
+ylabel('Magnitude (dB)')
+title('Ideal Range Response');
+
+%% Ideal Range Response w/o Zero Subcarrier
+% Number of subcarriers
+N = 64;
+
+% Frequency Axis
+k = 0:(N-1);
+k = k - N/2;
+
+% Determine Ideal Matched Filter Response
+% 1 at all non-zero subcarriers
+Y = zeros(size(k));
+Y(-26 <= k & k <= 26) = 1;
+
+% Get window for all non-zero subcarriers
+win = chebwin(53,80);
+padSize = N - length(win);
+win = [zeros(ceil(padSize/2),1); win; zeros(floor(padSize/2),1)].';
+
+% Apply window
+Y = Y.*win;
+
+%% Get range response
+% Determine the Range Response
+fftSize = 16*N;
+Ypad = [zeros(1, fftSize/2-N/2), Y, zeros(1, fftSize/2-N/2)];
+Yshift = ifftshift(Ypad);
+y = ifft(Yshift, fftSize);
+y = y/max(abs(y));
+n = (0:(length(y)-1));
+figure(6)
+clf;
+plot(n,db(y),'LineWidth',1.5);
+grid on;
+xlim([n(1) n(end)])
+xlabel('Sample')
+ylabel('Magnitude (dB)')
+title('Ideal Range Response');
